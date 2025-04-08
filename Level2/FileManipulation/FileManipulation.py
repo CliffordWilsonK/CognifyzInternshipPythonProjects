e447b318fb21d14c0911c0e43c100cc8d0322772
[Clifford Wilson] <[cliffordwilsonk@gmail.com]>
2025-04-08 15:07:17 +0000

    Done with task 2

--- Level2/FileManipulation/FileManipulation.py
+++ Level2/FileManipulation/FileManipulation.py
@@ -1,21 +1,20 @@
 import re
+import os
+print("Current Working Directory:", os.getcwd())
 
-file = open('file.txt', 'r')
+
+with open('text.txt', 'r') as file:
+    content = file.read()
+
 words = {}
-word = re.findall(r'a-zA-Z')
-for word in file.read():
+
+word_list = re.findall(r'\b[a-zA-Z]+\b', content)
+
+for word in word_list:
+    word = word.lower()
     if word in words:
         words[word] += 1
     else:
         words[word] = 1
-
-# with open('file.txt', 'r') as file:
-#     words = {}
-#     word = re.findall(r'a-zA-Z')
-#     for word in file.read():
-#         if word in words:
-#             words[word] += 1
-#         else:
-#             words[word] = 1
 
 print(words)
